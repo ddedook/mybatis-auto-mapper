b118b2422a7d77353ad33a4c97a3e73e1a640659
harris.ding <harris.ding@izeniu.com>
2016-12-21 20:41:29 +0800

    update order by.

---
+++ src/main/java/com/shzisg/mybatis/mapper/auto/OrderBy.java
@@ -0,0 +1,13 @@
+package com.shzisg.mybatis.mapper.auto;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface OrderBy {
+  
+  String value();
+  
+  boolean desc() default true;
+}
