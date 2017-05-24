7240a97b0c9ce39e136ada13b51076453fa2a97a
hao.ding <hao.ding@sumscope.com>
2017-05-24 14:19:02 +0800

    fix bug.

--- src/main/java/com/shzisg/mybatis/mapper/builder/DeleteBuilder.java
+++ src/main/java/com/shzisg/mybatis/mapper/builder/DeleteBuilder.java
@@ -35,7 +35,7 @@ public class DeleteBuilder implements SqlBuilder {
             } else {
                 builder.append(" and ")
                     .append(columnMap.get(param))
-                    .append(type.getParameterAnnotation(Not.class) != null ? "" : "!")
+                    .append(type.getParameterAnnotation(Not.class) != null ? "!" : "")
                     .append("=")
                     .append(MapperUtils.buildTypeValue(param, type.getParameterType(), "", typeHandlers.get(param)));
             }
